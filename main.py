6c74f59c1552ba06dd545a2c3e9707ce48930e1c
Yegor <kutuzov-1313@yadnex.ru>
2021-03-05 23:24:34 +0300

    hot fix fun 'download images' and add test download images

--- main.py
+++ main.py
@@ -3,27 +3,23 @@ import os
 import requests
 
 
-directory = './images'
-
-
-def get_images_spacex(link_spacex):
-    response = requests.get(link_spacex)
+def get_images_spacex():
+    api_spacex = 'https://api.spacexdata.com/v4/launches/latest'
+    response = requests.get(api_spacex)
     response.raise_for_status()
     images_spacex = response.json()['links']['flickr']['original']
     return images_spacex
 
 
-def download_images(link_image, image_name):
+def download_images(image_name, link_image):
+    directory = './images'
     response = requests.get(link_image)
     response.raise_for_status()
     if not os.path.exists(directory):
         os.makedirs(directory)
-        with open(f'{directory}/{image_name}', mode='wb') as image:
-            image.write(response.content)
+    with open(f'{directory}/{image_name}.jpeg', mode='wb') as pic:
+        pic.write(response.content)
 
 
-name = 'sputnik.jpeg'
-link_to_image = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-spacex = 'https://api.spacexdata.com/v4/launches/latest'
-# download_images(link_to_image, name)
-print(get_images_spacex(spacex))
+for image_number, image in enumerate(get_images_spacex()):
+    download_images(image_number, image)
