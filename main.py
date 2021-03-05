5b5242489464141180578826278519411fb3ff22
Yegor <kutuzov-1313@yadnex.ru>
2021-03-05 22:42:01 +0300

    add code for download images in func

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ import os
 
 import requests
 
-image_name = 'sputnik.jpeg'
-link_to_image = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+
 directory = './images'
 
-response = requests.get(link_to_image)
-response.raise_for_status()
-if not os.path.exists(directory):
-    os.makedirs(directory)
-with open(f'{directory}/{image_name}', mode='wb') as image:
-    image.write(response.content)
+
+def download_images(link_image, image_name):
+    response = requests.get(link_image)
+    response.raise_for_status()
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+        with open(f'{directory}/{image_name}', mode='wb') as image:
+            image.write(response.content)
+
+
+name = 'sputnik.jpeg'
+link_to_image = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+download_images(link_to_image, name)
