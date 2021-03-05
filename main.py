d419da013d166d1cddb5f3a8b880915584b8dd4d
Yegor <kutuzov-1313@yadnex.ru>
2021-03-05 22:05:23 +0300

    add requests.get for download image

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import requests
 
-name_image = ''
+image_name = 'sputnik.jpeg'
+link_to_image = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+
+response = requests.get(link_to_image)
+response.raise_for_status()
+
+with open(image_name, mode='wb') as image:
+    image.write(response.content)
