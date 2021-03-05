87cfc6fc4c5c9096b5961181fca6eaba04b66d68
Yegor <kutuzov-1313@yadnex.ru>
2021-03-05 22:30:08 +0300

    add func write file in dir, if dit missing create dir

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import os
+
 import requests
 
 image_name = 'sputnik.jpeg'
 link_to_image = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+directory = './images'
 
 response = requests.get(link_to_image)
 response.raise_for_status()
-
-with open(image_name, mode='wb') as image:
+if not os.path.exists(directory):
+    os.makedirs(directory)
+with open(f'{directory}/{image_name}', mode='wb') as image:
     image.write(response.content)
