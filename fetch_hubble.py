5ed05ba383df8f505a27868125de669f4fe08b45
Yegor <kutuzov-1313@yadnex.ru>
2021-03-07 16:33:16 +0300

    Packing the code

---
+++ fetch_hubble.py
@@ -0,0 +1,34 @@
+import argparse
+from pathlib import Path
+
+import requests
+
+from upload_insta import download_images, format_image
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('id', help='id запуска')
+    parser.add_argument('--download_path', help='Путь куда надо скачать изображения', default='images')
+    return parser
+
+
+def fetch_image_hubble():
+    parser = create_parser()
+    args = parser.parse_args()
+    hubble_launch_id = args.id
+    hubble_path_images = args.download_path
+    Path(hubble_path_images).mkdir(parents=True, exist_ok=True)
+    api_hubble = f'http://hubblesite.org/api/v3/image/{hubble_launch_id}'
+
+    response = requests.get(api_hubble)
+    response.raise_for_status()
+
+    last_image_hubble = response.json()['image_files'][-1]
+    last_image_url = f'https:{last_image_hubble["file_url"]}'
+    hubble_filename = f'hubble{hubble_launch_id}.{format_image(last_image_hubble["file_url"])}'
+    print(hubble_filename, last_image_url, hubble_path_images)
+
+
+if __name__ == '__main__':
+    fetch_image_hubble()
---
+++ fetch_spacex.py
@@ -0,0 +1,34 @@
+import argparse
+from pathlib import Path
+
+import requests
+
+from upload_insta import download_images, format_image
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('id', help='id запуска', default='latest')
+    parser.add_argument('--download_path', help='Путь куда надо скачивать изображения', default='images')
+    return parser
+
+
+def fetch_image_spacex():
+    parser = create_parser()
+    args = parser.parse_args()
+    spacex_launch_id = args.id
+    spacex_path_images = args.download_path
+    Path(spacex_path_images).mkdir(parents=True, exist_ok=True)
+    api_spacex = f'https://api.spacexdata.com/v4/launches/{spacex_launch_id}'
+
+    response = requests.get(api_spacex)
+    response.raise_for_status()
+
+    images_spacex_url = response.json()['links']['flickr']['original']
+    for image_names, spacex_url in enumerate(images_spacex_url):
+        spacex_filename = f'spacex{image_names}.{format_image(spacex_url)}'
+        download_images(spacex_filename, spacex_url, spacex_path_images)
+
+
+if __name__ == '__main__':
+    fetch_image_spacex()
